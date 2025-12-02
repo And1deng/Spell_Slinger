c806b54f7f747b6da30d09dc67bc7644e00e89af
And1deng <147691970+And1deng@users.noreply.github.com>
2025-12-02 15:50:44 -0800

    Init with temp graphics

---
+++ main.lua
@@ -0,0 +1,49 @@
+--Spell Slinger--
+
+require 'src/Dependencies'
+
+function love.load()
+    math.randomseed(os.time())
+    love.window.setTitle('SpellSlinger')
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        vsync = true,
+        resizable = true
+    })
+
+    --love.graphics.setFont(gFonts['small'])
+
+    gStateMachine = StateMachine {
+        ['start'] = function() return StartState() end,
+        ['play'] = function() return PlayState() end,
+        ['game-over'] = function() return GameOverState() end
+    }
+    gStateMachine:change('start')
+
+    love.keyboard.keysPressed = {}
+
+end
+
+function love.resize(w, h)
+    push:resize(w, h)
+end
+
+function love.keypressed(key)
+    love.keyboard.keysPressed[key] = true
+end
+
+function love.keyboard.wasPressed(key)
+    return love.keyboard.keysPressed[key]
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+    push:start()
+
+    push:finish()
+end
